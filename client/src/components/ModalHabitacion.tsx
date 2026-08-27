91263da34f22230b50b847ba72af196960bab821
Kevin <niichols1993@gmail.com>
2026-08-26 22:44:24 -0500

    feat: restore original square badge status icons for client Windows machines

--- client/src/components/ModalHabitacion.tsx
+++ client/src/components/ModalHabitacion.tsx
@@ -778,10 +778,10 @@ export const ModalHabitacion = ({
                       value={estado}
                       onChange={(e) => setEstado(e.target.value)}
                     >
-                      <option value="Disponible">🟢 Disponible</option>
-                      <option value="Reservada">🟡 Reservada</option>
-                      <option value="Ocupada">🔴 Ocupada</option>
-                      <option value="Inhabilitada">⚫ Inhabilitada</option>
+                      <option value="Disponible">🟩 Disponible</option>
+                      <option value="Reservada">🟧 Reservada</option>
+                      <option value="Ocupada">🟥 Ocupada</option>
+                      <option value="Inhabilitada">⬛ Inhabilitada</option>
                     </select>
                   </div>
 
